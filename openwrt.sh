495e25b2bc63d754d756d9f32ae5ddd6e1ab0090
Sheathan <quellodiabolico@gmail.com>
2020-10-09 23:18:56 -0400

    Update openwrt.sh

--- openwrt.sh
+++ openwrt.sh
@@ -2,6 +2,7 @@
 echo -e "\e[92mDownloading Image...\e[0m"
 wget https://github.com/Sheathan/Steelleaf/blob/master/vzdump-qemu-2000-2020_10_09-20_52_04.vma.zst
 mv vzdump-qemu-2000-2020_10_09-20_52_04.vma.zst /var/lib/vz/dump/
+sleep 2
 echo -e "\e[92mDownload complete, restoring template...\e[0m"
 qmrestore /var/lib/vz/dump/vzdump-qemu-2000-2020_10_09-20_52_04.vma.zst 8000
 echo -e "\e[92mSetting up template and deploying...\e[0m"
