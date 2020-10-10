3c51fe4679022aac72250fba5232389084b46715
Sheathan <quellodiabolico@gmail.com>
2020-10-09 23:07:42 -0400

    Update openwrt.sh

--- openwrt.sh
+++ openwrt.sh
@@ -1,13 +1,17 @@
+# Reference https://misc.flogisoft.com/bash/tip_colors_and_formatting for bash color usage
 wget https://github.com/Sheathan/Steelleaf/blob/master/vzdump-qemu-2000-2020_10_09-20_52_04.vma.zst /var/lib/vz/dump
 qmrestore /var/lib/vz/dump/vzdump-qemu-2000-2020_10_09-20_52_04.vma.zst 8000
+echo -e "\e[92mSetting up template and deploying...\e[0m"
 qm clone 8000 2000 --name TST-GW01
+echo -e "\e[92mDeployment complete, waiting to boot VM...\e[0m"
 sleep 10
+echo -e "\e[92mCloning complete, booting..."
 qm start 2000
-echo "Waiting 20 seconds for VM to boot..."
+echo -e "\e[92mWaiting 20 seconds for VM to boot...\e[0m"
 sleep 20
-echo "Configuring OpenWRT"
+echo -e "\e[92mConfiguring OpenWRT\e[0m"
 qm guest exec 2000 uci set network.lan.ipaddr=10.0.0.1
 qm guest exec 2000 uci commit
 qm guest exec 2000 reboot
-echo "VM configured, rebooting..."
-echo "Configuration complete, access OpenWRT on 10.0.0.1 after 30 seconds"
+echo -e "\e[92mVM configured, rebooting...\e[0m"
+echo -e "\e[92mConfiguration complete, access OpenWRT on 10.0.0.1 after 30 seconds\e[0m"
