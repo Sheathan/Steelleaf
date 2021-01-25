8d6ce00be585b37f436268ee94c1837f409b73ad
Sheathan <quellodiabolico@gmail.com>
2021-01-25 12:50:54 -0500

    Update install_bridge.sh

--- install_bridge.sh
+++ install_bridge.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 #Change CHANGEME to customer subnet number
 wget http://www.softether-download.com/files/softether/v4.34-9745-rtm-2020.04.05-tree/Linux/SoftEther_VPN_Bridge/64bit_-_Intel_x64_or_AMD64/softether-vpnbridge-v4.34-9745-rtm-2020.04.05-linux-x64-64bit.tar.gz
 apt update
